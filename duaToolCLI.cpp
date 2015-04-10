b6d8ba1cad6f916ce19f92fb3e306758c3a0f001
Jim O'Connor <joconno2@conncoll.edu>
2015-04-10 17:30:59 -0400

    Added XML formatting
    Program now prints in XML to log file

--- duaToolCLI.cpp
+++ duaToolCLI.cpp
@@ -3,23 +3,24 @@
 #include <boost/filesystem.hpp>
 using namespace boost::filesystem;
 
-void list(path,std::ofstream&);
+void list(path,std::ofstream&,int);
 
 
 int main(int argc, char* argv[]){
 	path p(argv[1]);   // p reads clearer than argv[1] in the following code
 	const char* filename = "log.txt";
+	int indentCount = 0;
 
 	std::ofstream logFile;
 	logFile.open(filename);
 
-	list(p,logFile);
+	list(p,logFile, indentCount);
 
 	logFile.close();
 }
 
 
-void list(path p, std::ofstream& logFile){
+void list(path p, std::ofstream& logFile, int indentCount){
 	try{
 		if (exists(p)){    // does p actually exist?
 
@@ -28,7 +29,12 @@ void list(path p, std::ofstream& logFile){
 
 			else if (is_directory(p))      // is p a directory?
 			{
-				logFile << p << " is a directory containing:\n";
+				for (int i = 0; i < indentCount; i++)
+					logFile << '\t';
+
+				logFile << "<Dir name= " << p << ">\n";
+
+				indentCount++;
 
 				typedef std::vector<path> vec;             // store paths,
 				vec v;                                // so we can sort them later
@@ -38,9 +44,18 @@ void list(path p, std::ofstream& logFile){
 
 				for (vec::const_iterator it(v.begin()); it != v.end(); ++it)
 					if (is_directory(*it))	// Only print size if not a directory, else recurse
-						list(*it,logFile);
-					else
-						logFile << "   " << *it << " --- " << file_size(*it) << '\n';
+						list(*it,logFile,indentCount);
+					else{
+						for (int i = 0; i < indentCount; i++)
+							logFile << '\t';
+						logFile << "<File>\n";
+						for (int i = 0; i < indentCount; i++)
+							logFile << '\t'; 
+						logFile << "\t<Path>" << *it << "<\Path>\n";
+						for (int i = 0; i < indentCount; i++)
+							logFile << '\t'; 
+						logFile << "\t<Size>" << file_size(*it) << "<\Size>\n";
+					}
 			}
 			else
 				std::cout << p << " exists, but is neither a regular file nor a directory\n";
